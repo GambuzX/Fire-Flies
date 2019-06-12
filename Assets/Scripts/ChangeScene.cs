007a66a682f67a9c44936a66e9a1c07f5a1f51f2
Mário Gil <mg_mesquita@hotmail.com>
2019-06-12 01:07:27 +0100

    initial file upload

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour { 
+public KeyCode key;
+
+
+    public void changeToScene(int changeTheScene)
+    {
+        SceneManager.LoadScene(changeTheScene);
+          
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(key))
+        {
+            SceneManager.LoadScene("2ndScreen");
+
+        }
+    }
+
+}
+   
---
+++ Assets/Scripts/ChangeToGame.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeToGame : MonoBehaviour
+{
+    public KeyCode key;
+
+
+
+    public void changeToScene(int changeTheScene)
+    {
+        SceneManager.LoadScene(changeTheScene);
+
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(key))
+        {
+            SceneManager.LoadScene("Game");
+
+        }
+
+    }
+}
